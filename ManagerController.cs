01178ddc0158fb6b93b55c8182d9cc3d35009ca4
Fatih <h.fatih_@outlook.com>
2021-01-20 21:23:06 +0300

    Proje dosyası ekle.

---
+++ ManagerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CarRentingSystem.BusinessLogic.Concretes;
+using CarRentingSystem.DataAccess.Entity;
+
+namespace CarRentingSystemApi.Controllers
+{
+    public class ManagerController : ApiController
+    {
+        // GET api/values
+        public HttpResponseMessage Get()
+        {
+            ManagerBusiness managerBusiness = new ManagerBusiness();
+
+            var tempManagers = managerBusiness.ListManagers().Select(
+                i => new
+                {
+                    i.Name,
+                    i.Address,
+                    i.BeginningDateOfDriverLicense,
+                    i.DatetimeOfCreated,
+                    i.CityOfBirth,
+                    i.PhotoURL,
+                }).ToList();
+
+            return Request.CreateResponse(HttpStatusCode.OK, tempManagers);
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public string Post(Companies entity)
+        {
+            try
+            {
+                CompanyBusiness companyBusiness = new CompanyBusiness();
+                bool result = companyBusiness.InsertCompany(entity);
+                return result == true ? "Added succesfuly!" : "Adding Failed!";
+            }
+            catch (Exception ex)
+            {
+                return "Adding failed! Exception : " + ex.Message;
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarRentingSystemApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public HttpResponseMessage Get()
+        {
+            List<String> list = new List<string>();
+            list.Add("Selam");
+            list.Add("hey");
+            return Request.CreateResponse(HttpStatusCode.OK, list);
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
